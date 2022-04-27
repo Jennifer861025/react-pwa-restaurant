31e0c898ce4393dd05d1819f16c1734c38044f58
Jennifer <jennifer861025@gmail.com>
2022-04-28 01:49:21 +0800

    feat: notification click_action

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -24,6 +24,7 @@ firebase.initializeApp(firebaseConfig);
 // Retrieve firebase messaging
 // eslint-disable-next-line no-undef
 const messaging = firebase.messaging();
+var click_action;
 
 messaging.onBackgroundMessage(function (payload) {
   console.log('Received background message ', payload);
@@ -33,10 +34,45 @@ messaging.onBackgroundMessage(function (payload) {
     body: payload.notification.body,
     icon: '/logo192.png',
   };
+  click_action =
+    notificationTitle == '用餐時間到'
+      ? 'https://react-pwa-restaurant-23ljkveff-jennifer861025.vercel.app/checkout'
+      : 'https://react-pwa-restaurant-23ljkveff-jennifer861025.vercel.app/waiting';
+  // click_action =
+  //   notificationTitle == '用餐時間到'
+  //     ? 'http://localhost:3000/checkout'
+  //     : 'http://localhost:3000/waiting';
+  // console.log('click_action1:' + JSON.stringify(payload.data));
 
   // eslint-disable-next-line no-restricted-globals
   return self.registration.showNotification(
     notificationTitle,
     notificationOptions,
+  );
+});
+
+// 監聽notifiction點擊事件
+self.addEventListener('notificationclick', function (event) {
+  console.log('click_action2:' + click_action);
+  var url = click_action;
+  event.notification.close();
+  event.waitUntil(
+    self.clients
+      .matchAll({
+        type: 'window',
+      })
+      .then((windowClients) => {
+        // 如果tab是開著的，就 focus 這個tab
+        for (var i = 0; i < windowClients.length; i++) {
+          var client = windowClients[i];
+          if (client.url === url && 'focus' in client) {
+            return client.focus();
+          }
+        }
+        // 如果沒有，就新增tab
+        if (self.clients.openWindow) {
+          return self.clients.openWindow(click_action);
+        }
+      }),
   );
 });
